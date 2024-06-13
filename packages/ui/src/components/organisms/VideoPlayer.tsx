633fad12859d865033eb4bb8fb759f2da90ef240
Undyingwraith <Undyingwraith@outlook.com>
2024-06-13 08:14:55 +0200

    Add VideoPlayer Hotkeys

--- packages/ui/src/components/organisms/VideoPlayer.tsx
+++ packages/ui/src/components/organisms/VideoPlayer.tsx
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { IVideoFile } from 'ipmc-interfaces';
 import { useProfile } from '../../context/ProfileContext';
+import { useHotkey } from '../../hooks';
 
 export function VideoPlayer(props: { file: IVideoFile; }) {
 	const { ipfs } = useProfile();
+	const videoRef = useRef<HTMLVideoElement>(null);
+
+	useHotkey({ key: 'F' }, () => {
+		videoRef.current?.requestFullscreen();
+	});
 
 	return (
-		<video controls style={{ height: '85vh', maxWidth: '100%', maxHeight: '100%' }}>
+		<video controls style={{ height: '85vh', maxWidth: '100%', maxHeight: '100%' }} ref={videoRef} preload={'metadata'}>
 			<source src={ipfs.toUrl(props.file.video.cid)}></source>
 		</video>
 	);
