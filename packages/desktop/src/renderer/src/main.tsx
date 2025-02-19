e217b363ef71bb465d0c1b9da385afb756e7aeed
UndyingWraith <undyingwraith@outlook.com>
2025-02-19 15:44:46 +0100

    Add loading text for workaround

--- packages/desktop/src/renderer/src/main.tsx
+++ packages/desktop/src/renderer/src/main.tsx
@@ -3,16 +3,8 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import 'ipmc-ui/style.css';
 
-function tryRender() {
-	if (!window.configService) {
-		setTimeout(tryRender, 100);
-	} else {
-		ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-			<React.StrictMode>
-				<App />
-			</React.StrictMode>
-		);
-	}
-}
-
-tryRender();
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>
+);
