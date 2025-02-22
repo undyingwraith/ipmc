813e0d58308bf798582c4e4832decb9da599315a
Undyingwraith <Undyingwraith@outlook.com>
2025-02-22 17:23:44 +0100

    Persist theme selection \#17

--- packages/ui/src/services/ThemeService.ts
+++ packages/ui/src/services/ThemeService.ts
@@ -1,10 +1,20 @@
 import { computed, Signal } from '@preact/signals-react';
+import { inject, injectable } from 'inversify';
+import { type IPersistentSignalService, IPersistentSignalServiceSymbol } from 'ipmc-interfaces';
 import { createDarkTheme, createLightTheme } from '../Theme';
 
 export const ThemeServiceSymbol = Symbol.for('ThemeService');
 
+@injectable()
 export class ThemeService {
-	public accentColor = new Signal('#6200EE');
-	public darkMode = new Signal(true);
+	constructor(
+		@inject(IPersistentSignalServiceSymbol) signalService: IPersistentSignalService,
+	) {
+		this.accentColor = signalService.get('accentColor', '#6200EE');
+		this.darkMode = signalService.get('darkMode', true);
+	}
+
+	public accentColor: Signal<string>;
+	public darkMode: Signal<boolean>;
 	public theme = computed(() => this.darkMode.value ? createDarkTheme(this.accentColor.value) : createLightTheme(this.accentColor.value));
 }
