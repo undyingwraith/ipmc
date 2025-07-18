599ccf0a58d2ebf1d40b2baabdf7f64a8eef0811
Undyingwraith <Undyingwraith@outlook.com>
2025-07-18 12:17:22 +0200

    Fix test

--- packages/ui/tests/components/atoms/FileInfoDisplay.test.tsx
+++ packages/ui/tests/components/atoms/FileInfoDisplay.test.tsx
@@ -1,10 +1,11 @@
-import React from 'react';
-import { describe, expect, test } from 'vitest';
 import { render } from '@testing-library/react';
 import { BrowserModule, CoreModule } from 'ipmc-core';
 import { IFileInfo, IIpfsServiceSymbol } from 'ipmc-interfaces';
+import React from 'react';
 import { FileInfoDisplay } from 'src/components/atoms/FileInfoDisplay';
 import { AppContextProvider } from 'src/context';
+import { describe, expect, test } from 'vitest';
+import { IObjectUrlControllerSymbol, ObjectUrlController } from '../../../src/services';
 
 describe('FileInfoDisplay', () => {
 	test('Renders data correctly', () => {
@@ -19,6 +20,7 @@ describe('FileInfoDisplay', () => {
 				app.use(CoreModule);
 				app.use(BrowserModule);
 				app.registerConstant({}, IIpfsServiceSymbol);
+				app.register(ObjectUrlController, IObjectUrlControllerSymbol);
 			}}>
 				<FileInfoDisplay file={fileInfo} />
 			</AppContextProvider>);
