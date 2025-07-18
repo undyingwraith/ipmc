e9231b2ba82c4bebe031955396268b4a5274a918
Undyingwraith <Undyingwraith@outlook.com>
2025-07-18 12:09:28 +0200

    Extract ObjectUrl logic to separate service

--- packages/ui/src/services/index.ts
+++ packages/ui/src/services/index.ts
@@ -1,4 +1,5 @@
 export * from './MediaPlayerService';
+export * from './ObjectUrlController';
 export { DialogService } from './DialogService';
 export { PopupService } from './PopupService';
 export { ThemeService, ThemeServiceSymbol } from './ThemeService';
