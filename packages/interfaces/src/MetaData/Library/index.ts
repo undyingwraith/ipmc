d5053db947ae8a8e285435f653270059e88f1961
Undyingwraith <Undyingwraith@outlook.com>
2025-08-17 20:21:34 +0200

    Improve index fetchers

--- packages/interfaces/src/MetaData/Library/index.ts
+++ packages/interfaces/src/MetaData/Library/index.ts
@@ -1,3 +1,4 @@
 export type { ILibrary } from './ILibrary';
 export type { IMovieMetaData } from './IMovieMetaData';
-export { isIEpisodeMetadata, isISeriesMetadata, type IEpisodeMetadata, type ISeasonMetadata, type ISeriesMetadata } from './ISeriesMetadata';
+export { isIEpisodeMetadata, isISeasonMetadata, isISeriesMetadata, type IEpisodeMetadata, type ISeasonMetadata, type ISeriesMetadata } from './ISeriesMetadata';
+
