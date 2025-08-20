48ebf328846aa6ab803b8a16500b0dc6815e31c7
Undyingwraith <Undyingwraith@outlook.com>
2025-08-20 04:51:01 +0200

    Fix routing for items with special characters in the name

--- packages/ui/src/IpmcApp.tsx
+++ packages/ui/src/IpmcApp.tsx
@@ -2,7 +2,7 @@ import { Box, CssBaseline } from '@mui/material';
 import { BrowserModule, CoreModule, FileExportService, IModule } from 'ipmc-core';
 import { IConfigurationService, IConfigurationServiceSymbol, IFileExportServiceSymbol, INodeService, ITranslation, ITranslationsSymbol } from 'ipmc-interfaces';
 import React from 'react';
-import { Router, Switch } from 'wouter';
+import { Router } from 'wouter';
 import { useHashLocation } from 'wouter/use-hash-location';
 import { AppBar } from './components/organisms/AppBar';
 import { AppContextProvider, ThemeProvider } from './context';
@@ -20,7 +20,10 @@ export function IpmcApp(props: IIpmcAppProps) {
 	const { setup } = props;
 
 	return (
-		<Router hook={useHashLocation}>
+		<Router hook={(options?: { ssrPath: string; }) => {
+			const [path, navigate] = useHashLocation(options);
+			return [decodeURIComponent(path), (url) => navigate(encodeURIComponent(url).replaceAll('%2F', '/'))];
+		}}>
 			<AppContextProvider setup={(app) => {
 				app.use(CoreModule);
 				app.use(BrowserModule);
@@ -35,9 +38,7 @@ export function IpmcApp(props: IIpmcAppProps) {
 					<Box sx={{ height: '100vh', width: '100vw', display: 'flex', flexDirection: 'column' }}>
 						<AppBar />
 						<Box sx={{ overflow: 'auto', flexGrow: 1 }}>
-							<Switch>
-								<IpmcLauncher configService={props.configService} nodeService={props.nodeService} />
-							</Switch>
+							<IpmcLauncher configService={props.configService} nodeService={props.nodeService} />
 						</Box>
 					</Box>
 				</ThemeProvider>
