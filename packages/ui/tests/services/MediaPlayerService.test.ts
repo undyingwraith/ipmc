8ea106afe1f8f547dcaf31b660e82d4d4daf4b3b
UndyingWraith <undyingwraith@outlook.com>
2025-08-30 08:18:36 +0200

    Add MediaPlayrService tests

---
+++ packages/ui/tests/services/MediaPlayerService.test.ts
@@ -0,0 +1,56 @@
+import { Application, LogService } from 'ipmc-core';
+import { ILogServiceSymbol, IVideoFile } from 'ipmc-interfaces';
+import { IMediaPlayerService, IMediaPlayerServiceSymbol, MediaPlayerService } from 'src/services';
+import { beforeEach, describe, expect, test } from 'vitest';
+
+describe('MediaPlayerService', () => {
+	let app: Application;
+	const testFile: IVideoFile = {
+		cid: 'test',
+		languages: [],
+		name: 'testName',
+		subtitles: [],
+		thumbnails: [],
+		type: 'dir',
+		video: {
+			cid: 'testCidVideo',
+			name: 'testVideoName',
+			type: 'file',
+		},
+		resolution: 0,
+		duration: 0,
+	};
+	const testFile2 = {
+		...testFile,
+		cid: 'test2',
+	};
+
+	beforeEach(() => {
+		app = new Application();
+		app.register(MediaPlayerService, IMediaPlayerServiceSymbol);
+		app.register(LogService, ILogServiceSymbol);
+	});
+
+	test('Can enqueue an item', () => {
+		const player = app.getService<IMediaPlayerService>(IMediaPlayerServiceSymbol)!;
+
+		player.enqueue(testFile);
+
+		expect(player.nowPlaying.value).not.toBeUndefined();
+		expect(player.queueIndex.value).toBe(0);
+		expect(player.queue.value.length).toBe(1);
+	});
+
+	test('Can enqueueNext', () => {
+		const player = app.getService<IMediaPlayerService>(IMediaPlayerServiceSymbol)!;
+
+		player.enqueue(testFile);
+		player.enqueue(testFile);
+		player.enqueueNext(testFile2);
+
+		expect(player.nowPlaying.value).not.toBeUndefined();
+		expect(player.queueIndex.value).toBe(0);
+		expect(player.queue.value.length).toBe(3);
+		expect(player.queue.value[player.queueIndex.value + 1].cid).toBe('test2');
+	});
+}); 
