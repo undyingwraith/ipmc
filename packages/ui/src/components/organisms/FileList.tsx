db95c625c78ff8a1249b13432b69c75473f692a6
Undyingwraith <Undyingwraith@outlook.com>
2025-09-09 18:10:25 +0200

    Add virtualized rendering for lists

--- packages/ui/src/components/organisms/FileList.tsx
+++ packages/ui/src/components/organisms/FileList.tsx
@@ -1,7 +1,6 @@
-import { Divider, List } from '@mui/material';
-import { interleave } from 'ipmc-core';
 import { IFileInfo } from 'ipmc-interfaces';
 import React from 'react';
+import { List } from 'react-window';
 import { useLocation } from 'wouter';
 import { ErrorBoundary, FileListItem } from '../molecules';
 
@@ -16,19 +15,24 @@ export function FileList(props: {
 	const [_, setLocation] = useLocation();
 
 	return (
-		<List>
-			{interleave(files.map((i, k) => (
-				<ErrorBoundary key={i.cid}>
-					<FileListItem
-						file={i}
-						onOpen={() => onOpen ? onOpen(i, k) : setLocation(`/${i.name}`)}
-						actions={actions ? actions(i, k) : undefined}
-						selected={selected === k}
-					/>
-				</ErrorBoundary>
-			)), (
-				<Divider />
-			))}
-		</List>
+		<List
+			rowComponent={({ files, index, style }) => {
+				const f = files[index];
+				return (
+					<ErrorBoundary key={f.cid}>
+						<FileListItem
+							style={style}
+							file={f}
+							onOpen={() => onOpen ? onOpen(f, index) : setLocation(`/${f.name}`)}
+							actions={actions ? actions(f, index) : undefined}
+							selected={selected === index}
+						/>
+					</ErrorBoundary>
+				);
+			}}
+			rowProps={{ files }}
+			rowCount={files.length}
+			rowHeight={90}
+		/>
 	);
 }
