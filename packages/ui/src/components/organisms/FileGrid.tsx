94ecb861e6648d2d7d6eb5194cbfe61172fa9435
UndyingWraith <undyingwraith@outlook.com>
2025-09-24 20:47:30 +0200

    Fix long titles overflowing

--- packages/ui/src/components/organisms/FileGrid.tsx
+++ packages/ui/src/components/organisms/FileGrid.tsx
@@ -1,4 +1,4 @@
-import { ReadonlySignal } from '@preact/signals-react';
+import { ReadonlySignal, useComputed } from '@preact/signals-react';
 import { IFileInfo } from 'ipmc-interfaces';
 import React from 'react';
 import AutoSizer from 'react-virtualized-auto-sizer';
@@ -11,52 +11,58 @@ export function FileGrid(props: { files: IFileInfo[]; display: ReadonlySignal<Di
 
 	const [_, setLocation] = useLocation();
 
-	return (
-		<AutoSizer>
-			{({ width, height }) => {
-				const finalWidth = width - 10;
-				const isMobile = finalWidth < 640;
-				const cardWidth = isMobile ? 150 : 240;
-				const cardHeight = isMobile ? 550 : 650;
+	return useComputed(() => {
+		const isPoster = display.value === Display.Poster;
 
-				const columnCount = Math.floor(finalWidth / cardWidth);
-				const rowCount = Math.ceil(files.length / columnCount);
+		return (
+			<AutoSizer>
+				{({ width, height }) => {
+					const finalWidth = width - 10;
+					const isMobile = finalWidth < 640;
+					const cardWidth = isMobile ? 150 : 240;
 
-				const wastedSpace = finalWidth - (cardWidth * columnCount);
-				const finalCardWidth = cardWidth + Math.floor(wastedSpace / columnCount);
+					const columnCount = Math.floor(finalWidth / cardWidth);
+					const rowCount = Math.ceil(files.length / columnCount);
 
-				return (
-					<div style={{ height: height, width: width }}>
-						<Grid
-							style={{ margin: '0 auto', padding: 5 }}
-							cellProps={{ files }}
-							cellComponent={({ files, rowIndex, columnIndex, style }) => {
-								const f = files[(rowIndex * columnCount) + columnIndex];
-								return (
-									<ErrorBoundary key={f?.cid}>
-										<div
-											style={{
-												...style,
-												padding: 5,
-											}}
-										>
-											<FileGridItem
-												file={f}
-												onOpen={() => setLocation(`/${f.name}`)}
-												display={display}
-											/>
-										</div>
-									</ErrorBoundary>
-								);
-							}}
-							columnCount={columnCount}
-							rowCount={rowCount}
-							columnWidth={finalCardWidth}
-							rowHeight={cardHeight}
-						/>
-					</div>
-				);
-			}}
-		</AutoSizer>
-	);
+					const wastedSpace = finalWidth - (cardWidth * columnCount);
+					const finalCardWidth = cardWidth + Math.floor(wastedSpace / columnCount);
+
+					const cardHeight = finalCardWidth * (isPoster ? 1.5 : 0.6) + 230;
+
+
+					return (
+						<div style={{ height: height, width: width }}>
+							<Grid
+								style={{ margin: '0 auto', padding: 5 }}
+								cellProps={{ files }}
+								cellComponent={({ files, rowIndex, columnIndex, style }) => {
+									const f = files[(rowIndex * columnCount) + columnIndex];
+									return (
+										<ErrorBoundary key={f?.cid}>
+											<div
+												style={{
+													...style,
+													padding: 5,
+												}}
+											>
+												<FileGridItem
+													file={f}
+													onOpen={() => setLocation(`/${f.name}`)}
+													display={display}
+												/>
+											</div>
+										</ErrorBoundary>
+									);
+								}}
+								columnCount={columnCount}
+								rowCount={rowCount}
+								columnWidth={finalCardWidth}
+								rowHeight={cardHeight}
+							/>
+						</div>
+					);
+				}}
+			</AutoSizer>
+		);
+	});
 }
