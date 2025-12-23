dc6090ba893d5f01ac5d67d4be9aba1b04742080
Undyingwraith <Undyingwraith@outlook.com>
2025-12-23 14:27:26 +0100

    Update helia/libp2p

--- packages/desktop/src/preload/index.ts
+++ packages/desktop/src/preload/index.ts
@@ -2,9 +2,11 @@ import { gossipsub } from '@chainsafe/libp2p-gossipsub';
 import { noise, pureJsCrypto } from '@chainsafe/libp2p-noise';
 import { yamux } from '@chainsafe/libp2p-yamux';
 import { bitswap, trustlessGateway } from '@helia/block-brokers';
+import { delegatedHTTPRouting, httpGatewayRouting, libp2pRouting } from '@helia/routers';
 import { autoNAT } from '@libp2p/autonat';
 import { bootstrap } from '@libp2p/bootstrap';
 import { circuitRelayTransport } from '@libp2p/circuit-relay-v2';
+import { loadOrCreateSelfKey } from '@libp2p/config';
 import { dcutr } from '@libp2p/dcutr';
 import { identify, identifyPush } from '@libp2p/identify';
 import { kadDHT, removePrivateAddressesMapper, removePublicAddressesMapper } from '@libp2p/kad-dht';
@@ -25,6 +27,9 @@ import fs from 'fs/promises';
 import { createHelia } from 'helia';
 import { createHeliaIpfs, Defaults } from 'ipmc-core';
 import { IConfigurationService, IInternalProfile, IIpfsService, INodeService, IProfile } from 'ipmc-interfaces';
+import { ipnsSelector } from 'ipns/selector';
+import { ipnsValidator } from 'ipns/validator';
+import { createLibp2p } from 'libp2p';
 import path from 'path';
 
 async function getProfileFolder(id?: string): Promise<string> {
@@ -41,98 +46,125 @@ const nodeService: INodeService = {
 		}
 
 		const folder = await getProfileFolder(profile.id);
+		const isPrivate = profile.swarmKey != undefined;
 
+		// Configure and start libp2p
 		const agentVersion = `ipmc-desktop/${__VERSION__}`;
 		const datastore = new LevelDatastore(path.join(folder, 'data'));
 		await datastore.open();
+		const privateKey = await loadOrCreateSelfKey(datastore);
+		const libp2p = await createLibp2p({
+			start: true,
+			datastore,
+			privateKey,
+			nodeInfo: {
+				userAgent: agentVersion
+			},
+			addresses: {
+				listen: [
+					`/ip4/0.0.0.0/tcp/${profile.port ?? 0}`,
+					`/ip6/::/tcp/${profile.port ?? 0}`,
+					`/ip4/0.0.0.0/udp/${profile.port ?? 0}/quic-v1`,
+					`/ip6/::/udp/${profile.port ?? 0}/quic-v1`,
+					`/ip4/0.0.0.0/udp/${profile.port ?? 0}/webrtc-direct`,
+					`/ip6/::/udp/${profile.port ?? 0}/webrtc-direct`,
+					'/ip4/0.0.0.0/tcp/0/ws',
+					'/ip6/::/tcp/0/ws',
+					'/p2p-circuit',
+				],
+			},
+			...(profile.swarmKey ? {
+				connectionProtector: preSharedKey({
+					psk: new TextEncoder().encode(profile.swarmKey),
+				}),
+			} : {}),
+			transports: [
+				webSockets(),
+				tcp(),
+				circuitRelayTransport(),
+				webRTC(),
+				webRTCDirect(),
+			],
+			peerDiscovery: [
+				bootstrap({
+					list: profile.bootstrap ?? (isPrivate ? [] : Defaults.Bootstrap),
+				}),
+				pubsubPeerDiscovery(),
+				mdns(),
+			],
+			connectionEncrypters: [
+				noise({
+					crypto: pureJsCrypto
+				}),
+			],
+			streamMuxers: [
+				yamux(),
+			],
+			services: {
+				lanDHT: kadDHT({
+					protocol: '/ipfs/lan/kad/1.0.0',
+					peerInfoMapper: removePublicAddressesMapper,
+					clientMode: false,
+					logPrefix: 'libp2p:dht-lan',
+					datastorePrefix: '/dht-lan',
+					metricsPrefix: 'libp2p_dht_lan',
+					validators: {
+						ipns: ipnsValidator
+					},
+					selectors: {
+						ipns: ipnsSelector
+					},
+				}),
+				aminoDHT: kadDHT({
+					protocol: '/ipfs/kad/1.0.0',
+					peerInfoMapper: removePrivateAddressesMapper,
+					logPrefix: 'libp2p:dht-amino',
+					datastorePrefix: '/dht-amino',
+					metricsPrefix: 'libp2p_dht_amino',
+					validators: {
+						ipns: ipnsValidator
+					},
+					selectors: {
+						ipns: ipnsSelector
+					},
+				}),
+				identify: identify(),
+				identifyPush: identifyPush(),
+				keychain: keychain(),
+				ping: ping(),
+				autoNAT: autoNAT(),
+				dcutr: dcutr(),
+				upnp: uPnPNAT(),
+				pubsub: gossipsub({
+					canRelayMessage: true,
+					enabled: true,
+				}),
+			},
+		});
+		libp2p.addEventListener('peer:discovery', console.log);
+		libp2p.addEventListener('peer:connect', console.log);
+		console.log(libp2p.getMultiaddrs().map(a => a.toString()));
+
+		// Configure and start helia
 		const blockstore = new FsBlockstore(path.join(folder, 'blocks'));
 		await blockstore.open();
-
 		const helia = await createHelia({
 			start: true,
 			datastore,
 			blockstore,
-			libp2p: {
-				nodeInfo: {
-					userAgent: agentVersion
-				},
-				addresses: {
-					listen: [
-						`/ip4/0.0.0.0/tcp/${profile.port ?? 0}`,
-						`/ip6/::/tcp/${profile.port ?? 0}`,
-						`/ip4/0.0.0.0/udp/${profile.port ?? 0}/quic-v1`,
-						`/ip6/::/udp/${profile.port ?? 0}/quic-v1`,
-						`/ip4/0.0.0.0/udp/${profile.port ?? 0}/webrtc-direct`,
-						`/ip6/::/udp/${profile.port ?? 0}/webrtc-direct`,
-						'/ip4/0.0.0.0/tcp/0/ws',
-						'/ip6/::/tcp/0/ws',
-						'/p2p-circuit',
-					],
-				},
-				...(profile.swarmKey ? {
-					connectionProtector: preSharedKey({
-						psk: new TextEncoder().encode(profile.swarmKey),
-					}),
-				} : {}),
-				transports: [
-					webSockets(),
-					tcp(),
-					circuitRelayTransport(),
-					webRTC(),
-					webRTCDirect(),
-				],
-				peerDiscovery: [
-					bootstrap({
-						list: profile.bootstrap ?? (profile.swarmKey != undefined ? [] : Defaults.Bootstrap),
-					}),
-					pubsubPeerDiscovery(),
-					mdns(),
-				],
-				connectionEncrypters: [
-					noise({
-						crypto: pureJsCrypto
-					}),
-				],
-				streamMuxers: [
-					yamux(),
-				],
-				services: {
-					lanDHT: kadDHT({
-						protocol: '/ipfs/lan/kad/1.0.0',
-						peerInfoMapper: removePublicAddressesMapper,
-						clientMode: false,
-						logPrefix: 'libp2p:dht-lan',
-						datastorePrefix: '/dht-lan',
-						metricsPrefix: 'libp2p_dht_lan'
-					}),
-					aminoDHT: kadDHT({
-						protocol: '/ipfs/kad/1.0.0',
-						peerInfoMapper: removePrivateAddressesMapper,
-						logPrefix: 'libp2p:dht-amino',
-						datastorePrefix: '/dht-amino',
-						metricsPrefix: 'libp2p_dht_amino'
-					}),
-					identify: identify(),
-					identifyPush: identifyPush(),
-					keychain: keychain(),
-					ping: ping(),
-					autoNAT: autoNAT(),
-					dcutr: dcutr(),
-					upnp: uPnPNAT(),
-					pubsub: gossipsub({
-						canRelayMessage: true,
-					}),
-				},
-			},
+			libp2p,
 			blockBrokers: [
 				bitswap(),
-				...(profile.swarmKey == undefined ? [trustlessGateway()] : [])
+				...(!isPrivate ? [trustlessGateway()] : [])
+			],
+			routers: [
+				libp2pRouting(libp2p),
+				...(!isPrivate ? [
+					delegatedHTTPRouting({ url: 'https://delegated-ipfs.dev' }),
+					httpGatewayRouting(),
+				] : [])
 			],
 		});
-
-		console.log(helia.libp2p.getMultiaddrs().map(a => a.toString()));
-		helia.libp2p.addEventListener('peer:discovery', console.log);
-		helia.libp2p.addEventListener('peer:connect', console.log);
 
 		const service = createHeliaIpfs(helia, async () => {
 			await blockstore.close();
